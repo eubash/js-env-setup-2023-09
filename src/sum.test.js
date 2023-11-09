9dd847cb6b7d3b34a577d96de797cf091fd91bc5
eubash <nemezidaxxx@mail.ru>
2023-11-09 19:43:20 +0100

    add prettier

--- src/sum.test.js
+++ src/sum.test.js
@@ -1,7 +1,7 @@
 import { sum } from "./sum";
 
-describe('sum', () => {
-  it('return sum', () => {
+describe("sum", () => {
+  it("return sum", () => {
     expect(sum(1, 2)).toEqual(3);
-  })
-})
+  });
+});
